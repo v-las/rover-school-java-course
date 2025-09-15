14f7fb84783d0d466eead6836b574d6cc0292212
Vlas Mastyksh <mastykash.vlas@gmail.com>
2025-09-16 00:17:00 +0600

    Small fix

--- src/homeworks/homework008/Employee.java
+++ src/homeworks/homework008/Employee.java
@@ -10,14 +10,35 @@ package homeworks.homework008;
  *
  * This class can be used in payroll and tax calculations.
  */
-public class Employee {
-    String name;
+public class Employee extends Person {
     int salary;
     int childrenCount;
 
-    Employee(String name, int salary, int childrenCount) {
-        this.name = name;
+    public Employee(String firstName, String lastName, int age, String gender, int salary, int childrenCount) {
+        super(firstName, lastName, age, gender);
         this.salary = salary;
         this.childrenCount = childrenCount;
+    }
+
+    public Employee(String lastName, int salary, int childrenCount) {
+        super(lastName);
+        this.salary = salary;
+        this.childrenCount = childrenCount;
+        this.gender = "male";
+    }
+
+    public boolean isSameName(Employee employee) {
+        return employee.lastName.equals(this.lastName);
+    }
+
+    public String getName() {
+        // переопределение метода из Person
+        if (gender.equals("male")) {
+            return "Employee Mr." + lastName;
+        } else if (gender.equals("female")) {
+            return "Employee Mrs." + lastName;
+        } else {
+            return "Can be 'male' or 'female'";
+        }
     }
 }
